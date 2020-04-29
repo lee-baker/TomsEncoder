c9c8b70032c29ab12ad3893e1605f82333a0139c
Lee Baker <lee.rekab@gmail.com>
2020-04-29 23:04:56 +0800

    Adding copy button, some more styling

--- app.js
+++ app.js
@@ -102,11 +102,56 @@ function decodeNew(message){
   return result; // LZString.decompressFromBase64(result);
 }
 
+var encoding = { messageThatWasEncoded: '',  encodedMessage: '', isNewEncoding: true};
+
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Stranger',
     encoded: 'J4QXX[j?`^MZSQ^',
     useNew: true
+  },
+  computed:{
+    messageEncoded: function() {
+      if (this.message != encoding.messageThatWasEncoded || this.useNew != encoding.isNewEncoding){
+        encoding.messageThatWasEncoded = this.message;
+        encoding.isNewEncoding = this.useNew;
+        encoding.encodedMessage = this.useNew ? encodeNew(this.message) : encode(this.message);
+      }
+      return encoding.encodedMessage;
+    }
   }
 })
+
+
+var copyToClipboard = function copyToClipboard(str) {
+  var el = document.createElement('textarea'); // Create a <textarea> element
+
+  el.value = str; // Set its value to the string that you want copied
+
+  el.setAttribute('readonly', ''); // Make it readonly to be tamper-proof
+
+  el.style.position = 'absolute';
+  el.style.left = '-9999px'; // Move outside the screen to make it invisible
+
+  document.body.appendChild(el); // Append the <textarea> element to the HTML document
+
+  var selected = document.getSelection().rangeCount > 0 // Check if there is any content selected previously
+  ? document.getSelection().getRangeAt(0) // Store selection if found
+  : false; // Mark as false to know no selection existed before
+
+  el.select(); // Select the <textarea> content
+
+  document.execCommand('copy'); // Copy - only works as a result of a user action (e.g. click events)
+
+  document.body.removeChild(el); // Remove the <textarea> element
+
+  if (selected) {
+    // If a selection existed before copying
+    document.getSelection().removeAllRanges(); // Unselect everything on the HTML document
+
+    document.getSelection().addRange(selected); // Restore the original selection
+  }
+};
+
+feather.replace();
